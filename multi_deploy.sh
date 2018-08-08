9a64a2f531d7de1b420057c84a85eef5ee48a538
priyankachennupati <40577708+priyankachennupati@users.noreply.github.com>
2018-08-08 18:24:00 +0530

    Update multi_deploy.sh

--- multi_deploy.sh
+++ multi_deploy.sh
@@ -8,7 +8,7 @@ if [ $ENVIRONMENT = "QA" ];then
 #
 	echo "Deploying war file into $i QA server.."
 	sleep 3
-	sshpass -p "priya1@" scp target/gamutkart.war gamut@$i:/home/priya/apache-tomcat-8.5.32/webapps
+	sshpass -p "priya1@" scp target/gamutkart.war priya@$i:/home/priya/apache-tomcat-8.5.32/webapps
 #
 	echo "Starting tomcat server in $i QA server.."
 	sshpass -p "priya1@" ssh priya@$i "JAVA_HOME=/home/priya/jdk1.8.0_172" "/home/priya/apache-tomcat-8.5.32/bin/startup.sh"
